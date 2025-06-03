d5c56099b1be79dca701a4b1982ce829867586ff
Arjunstein <arjun.gnw09@gmail.com>
2025-06-03 14:44:14 +0700

    ⚡️ set route and pages

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>About Us</h1>
+        <p>Welcome to the About page, {{ user }}!</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    user: String
+})
+</script>
